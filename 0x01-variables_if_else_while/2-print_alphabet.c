60103c42dd5d1d11f8bdca4d70476f8f07878ebe
Melusi Maleko <melusimaleko@gmail.com>
2023-02-17 13:28:42 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,11 +13,12 @@ int main(void)
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
-	{
-        putchar(low);
-        putchar('');
 
-	}
+        putchar(low);
+
+
+
+	putchar('\n');
 
 	return (0);
 }
