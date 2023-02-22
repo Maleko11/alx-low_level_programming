95dd95f91c5517b99c6342095bfd96ca8a9ebc91
Melusi Maleko <melusimaleko@gmail.com>
2023-02-22 12:56:51 +0200

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,24 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * jack_bauer - Check Holberton
+ * jack_bauer - Check main
  * Description: function prints every minute in one day
  * Return: Nothing
  */
 void jack_bauer(void)
 {
-	int H, M;
+int H, M;
 
-	for (H = 0; H < 24; H++)
-	{
-		for (M = 0; M < 60; M++)
-		{
-			_putchar((H / 10) + '0');
-			_putchar((H % 10) + '0');
-			_putchar(':');
-			_putchar((M / 10) + '0');
-			_putchar((M % 10) + '0');
-			_putchar('\n');
-		}
-	}
+for (H = 0; H < 24; H++)
+{
+for (M = 0; M < 60; M++)
+{
+_putchar((H / 10) + '0');
+_putchar((H % 10) + '0');
+_putchar(':');
+_putchar((M / 10) + '0');
+_putchar((M % 10) + '0');
+_putchar('\n');
+}
+}
 }
