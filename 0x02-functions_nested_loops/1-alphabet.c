64dd66b1207a3a58be727d036a83125e68e5912e
Melusi Maleko <melusimaleko@gmail.com>
2023-02-22 12:55:01 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,15 @@
 #include "main.h"
+
 /**
- * main - Entry point
- * Return: Always 0
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
  */
-int main(void)
+
+void print_alphabet(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+char i;
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
 _putchar('\n');
-return (0);
 }
