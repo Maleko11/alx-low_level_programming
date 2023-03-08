9202a22463e663d5e5b923472429c8bc3f4a8f98
Melusi Maleko <melusimaleko@gmail.com>
2023-03-08 15:10:28 +0200

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*_print_rev_recursion - prints char in revers
+*@s: is a stirng input to be printed backwards
+*/
+void _print_rev_recursion(char *s)
+{
+int len = strlen(s);
+for(int i = len -1; i >= 0; i--)
+{
+    printf("%c",s[i]);
+}
+printf("\n");
+}
+
