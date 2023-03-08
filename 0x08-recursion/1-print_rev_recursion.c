119d4be80c7e564fa980b70631c4e431f2e16d05
Melusi Maleko <melusimaleko@gmail.com>
2023-03-08 15:44:05 +0200

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,11 +8,11 @@
 void _print_rev_recursion(char *s)
 {
 
-int len = strlen(s);
-int i = len -1;
-for(i ; i >= 0; i--)
-{
-    printf("%c",s[i]);
+    int len = strlen(s);
+    int i = len -1;
+    for(i ; i >= 0; i--)
+    {
+        printf("%c",s[i]);
 }
 printf("\n");
 }
