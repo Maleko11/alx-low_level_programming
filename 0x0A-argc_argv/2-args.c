9d91ef9ebe268114941e5342a259c0576178eeae
Melusi Maleko <melusimaleko@gmail.com>
2023-03-10 11:40:11 +0200

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#inclide <stdio.h>
+#include <libgen.h>
+
+/**
+*main - prints the number of arguments it recieves
+*@argc: contains number of arguments
+*@artgv: contain the list of arguments
+*/
+int main(int argc, char *argv[])
+{
+    for(int i = 0, i <= argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+return 0;
+}
