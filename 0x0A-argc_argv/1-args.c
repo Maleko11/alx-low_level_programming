27fe2c9a4440d840fd48319d1b39aa3e4387e0de
Melusi Maleko <melusimaleko@gmail.com>
2023-03-10 11:26:54 +0200

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#inclide <stdio.h>
+#include <libgen.h>
+
+/**
+*main - prints the number of arguments
+*@argc: contains number of arguments
+*@artgv: contain the list of arguments
+*/
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return 0;
+}
