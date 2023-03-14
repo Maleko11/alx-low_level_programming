3acb54864bf3a6ec4bfde8d725dc3fcfff123639
Melusi Maleko <melusimaleko@gmail.com>
2023-03-14 15:27:45 +0200

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * argstostr - concatenates all arguments of your program
@@ -9,37 +9,37 @@
 
 char *argstostr(int ac, char **av)
 {
-	int i, j, len = 0, k = 0;
-	char *str;
+int i, j, len = 0, k = 0;
+char *str;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
 
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-			len++;
-		len++;
-	}
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+len++;
+len++;
+}
 
-	str = malloc(sizeof(char) * (len + 1));
+str = malloc(sizeof(char) * (len + 1));
 
-	if (str == NULL)
-	{
-		free(str);
-		return (NULL);
-	}
+if (str == NULL)
+{
+free(str);
+return (NULL);
+}
 
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
-		str[k] = '\n';
-		k++;
-	}
-	str[k] = '\0';
-	return (str);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+str[k] = av[i][j];
+k++;
+}
+str[k] = '\n';
+k++;
+}
+str[k] = '\0';
+return (str);
 }
