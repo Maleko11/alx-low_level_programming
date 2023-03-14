ee6cee20f0f8277c4eee0d7ba93806956025e72f
Melusi Maleko <melusimaleko@gmail.com>
2023-03-14 14:44:42 +0200

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,18 +11,18 @@
  */
 char *_strdup(char *str)
 {
-    char *new_str;
-	if (str == NULL)
-	{
-		return (NULL);
-	}
+char *new_str;
+if (str == NULL)
+{
+return (NULL);
+}
 
-	 new_str = malloc(strlen(str) + 1);
-	if (new_str == NULL)
-	{
-		return (NULL);
-	}
+new_str = malloc(strlen(str) + 1);
+if (new_str == NULL)
+{
+return (NULL);
+}
 
-	strcpy(new_str, str);
-	return (new_str);
+strcpy(new_str, str);
+return (new_str);
 }
