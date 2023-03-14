9660e574d72a22386e808c62ec97fe3decac23a6
Melusi Maleko <melusimaleko@gmail.com>
2023-03-14 13:34:35 +0200

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,14 +8,14 @@
 
 char *create_array(unsigned int size, char c)
 {
-if(size == NULL)
+if(size == 0)
 {
-return 0;
+return NULL;
 }
 char *arr = (char*) malloc(size * sizeof(char));
 
-if(arr == NULL){
-return 0;
+if(arr == 0){
+return NULL;
 }
 
 for(unsigned int i = 0; i < size; i++)
