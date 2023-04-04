2e777c8bae4ebc17a1bb7feea43af036133d509b
Melusi Maleko <melusimaleko@gmail.com>
2023-04-04 10:22:01 +0200

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,13 +9,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
- size_t count = 0;
+ size_t add = 0;
 
 while (h != NULL)
 {
-count++;
+add= add + 1;
 h = h->next;
 }
 
-return (count);
+return (add);
 }
